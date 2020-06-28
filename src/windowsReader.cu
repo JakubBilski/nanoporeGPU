1f7b6a39e3b99aef7573b17911d4092c3e340dc7
JakubBilski <jakub.michal.bilski@gmail.com>
2020-06-28 19:27:14 +0200

    Add graph costruction from clean data file
    Also changed target so it works on vs2017 and (hopefully) 2019

--- src/windowsReader.cu
+++ src/windowsReader.cu
@@ -17,7 +17,12 @@
 #include "defines.h"
 #include "Correct.h"
 
-template <int TNoBlocks> int* precleanedJumpGPU(std::fstream& fs, std::fstream& ts, int& size);
+template <int TNoBlocks>
+int* precleanedToGraph(std::fstream& fs, int& out_treeLength);
+
+template <int TNoBlocks>
+int* fastqToGraphAndPrecleaned(std::fstream& fs, std::fstream& ts, int& out_treeLength);
+
 
 int main(int argc, char* argv[])
 {
@@ -54,8 +59,8 @@ int main(int argc, char* argv[])
 		clock_t start = clock();
 
 		int DBG_size = 0;
-		int* DBG = precleanedJumpGPU<10>(fs, ts, DBG_size);
-		printf("%25s = %11f\n", "precleanedJumpGPU<10>", 0.001f * (clock() - start) * 1000 / CLOCKS_PER_SEC);
+		int* DBG = fastqToGraphAndPrecleaned<10>(fs, ts, DBG_size);
+		printf("%25s = %11f\n", "fastqToGraphAndPrecleaned<10>", 0.001f * (clock() - start) * 1000 / CLOCKS_PER_SEC);
 		fs.close();
 		ts.close();
 
@@ -72,6 +77,27 @@ int main(int argc, char* argv[])
 		fs.close();
 		ts.close();
 
+		fs.open(tempFilePath2, std::ios::in | std::ios::binary);
+		assertOpenFile(fs, tempFilePath);
+		start = clock();
+
+		DBG = precleanedToGraph<10>(fs, DBG_size);
+		printf("%25s = %11f\n", "precleanedToGraph<10>", 0.001f * (clock() - start) * 1000 / CLOCKS_PER_SEC);
+		fs.close();
+
+		char* tempFilePath3 = "result_file_aa2.txt";
+		fs.open(tempFilePath2, std::ios::in | std::ios::binary);
+		assertOpenFile(fs, tempFilePath);
+		ts.open(tempFilePath3, std::ios::in | std::ios::out | std::ios::binary | std::ofstream::trunc);
+		assertOpenFile(ts, tempFilePath2);
+		start = clock();
+
+		std::vector<int> DBG_v2(DBG, DBG + DBG_size);
+		Correct(fs, ts, DBG_v2, MER_LENGHT);
+		printf("%25s = %11f\n", "Correct", 0.001f * (clock() - start) * 1000 / CLOCKS_PER_SEC);
+		fs.close();
+		ts.close();
+
 		printf("\n");
 	}
 	return 0;
@@ -79,7 +105,88 @@ int main(int argc, char* argv[])
 }
 
 template <int TNoBlocks>
-int* precleanedJumpGPU(std::fstream& fs, std::fstream& ts, int& size)
+int* precleanedToGraph(std::fstream& fs, int& out_treeLength)
+{
+	char* d_chunk;
+	int* d_tree;
+	int* d_treeLength;
+	char* chunk = (char*)malloc(sizeof(char)*HOST_CHUNK_SIZE);
+	gpuErrchk(cudaMalloc(&d_chunk, DEVICE_CHUNK_SIZE * sizeof(char)));
+	gpuErrchk(cudaMalloc(&d_tree, DEVICE_TREE_SIZE * sizeof(int)));
+	gpuErrchk(cudaMemset(d_tree, 0, DEVICE_TREE_SIZE * sizeof(int)));
+	gpuErrchk(cudaMalloc(&d_treeLength, sizeof(int)));
+	const int startingTreeLength = 4;
+	gpuErrchk(cudaMemcpy(d_treeLength, &startingTreeLength, sizeof(int), cudaMemcpyHostToDevice));
+	int chunkOffset = 0;
+	int lastNewline = 0;
+	int loadedToGPU = 0;
+	fs.read(chunk + chunkOffset, HOST_CHUNK_SIZE - chunkOffset);
+	int chunkLength = chunkOffset + fs.gcount();
+	bool readEnded = false;
+	while (!readEnded)
+	{
+		//w gpu zmiesci sie wiecej niz jest w buforze
+		if (chunkLength - 1 < lastNewline + (DEVICE_CHUNK_SIZE - loadedToGPU))
+		{
+			int i = chunkLength - 1;
+			while (chunk[i] != '\n')
+			{
+				i--;
+			}
+			//skopiuj dane do gpu, ale nie odpalaj kernela
+			gpuErrchk(cudaMemcpy(d_chunk + loadedToGPU, chunk + lastNewline, (i - lastNewline) * sizeof(char), cudaMemcpyHostToDevice));
+			loadedToGPU += (i - lastNewline);
+
+			//skopiuj ostatni urwany read w buforze na poczatek bufora
+			memcpy(chunk, chunk + i, sizeof(char)*(chunkLength - i));
+			chunkOffset = chunkLength - i;
+
+			if (!fs.eof())
+			{
+				//wczytaj z pliku az do wypelnienia bufora
+				fs.read(chunk + chunkOffset, HOST_CHUNK_SIZE - chunkOffset);
+				chunkLength = chunkOffset + fs.gcount();
+				lastNewline = 0;
+			}
+			else
+			{
+				//plik sie skonczyl, odpalamy kernel pomimo niewypelnienia do konca pamieci gpu
+				AddPrecleanedChunkToGraph<MER_LENGHT> << <TNoBlocks, BLOCK_SIZE >> > (TNoBlocks, d_chunk, loadedToGPU, d_tree, d_treeLength);
+				kernelErrchk();
+				readEnded = true;
+			}
+		}
+		else  //w gpu zmiesci sie mniej niz jest w buforze
+		{
+			int i = lastNewline + (DEVICE_CHUNK_SIZE - loadedToGPU);
+			while (chunk[i] != '\n')
+			{
+				i--;
+			}
+			//skopiuj ile sie zmiesci do gpu i odpal kernel
+			gpuErrchk(cudaMemcpy(d_chunk + loadedToGPU, chunk + lastNewline, (i - lastNewline) * sizeof(char), cudaMemcpyHostToDevice));
+			AddPrecleanedChunkToGraph<MER_LENGHT> << <TNoBlocks, BLOCK_SIZE >> > (TNoBlocks, d_chunk, loadedToGPU + i - lastNewline, d_tree, d_treeLength);
+			kernelErrchk();
+			loadedToGPU = 0;
+			lastNewline = i;
+		}
+	}
+	DeleteWeakLeaves<MER_LENGHT> << <TNoBlocks, BLOCK_SIZE >> > (TNoBlocks, d_tree);
+	kernelErrchk();
+	int finalTreeLength = 0;
+	gpuErrchk(cudaMemcpy(&finalTreeLength, d_treeLength, sizeof(int), cudaMemcpyDeviceToHost));
+	int* finalTree = (int*)malloc(sizeof(int)*finalTreeLength);
+	gpuErrchk(cudaMemcpy(finalTree, d_tree, finalTreeLength * sizeof(int), cudaMemcpyDeviceToHost));
+	gpuErrchk(cudaFree(d_chunk));
+	gpuErrchk(cudaFree(d_tree));
+	gpuErrchk(cudaFree(d_treeLength));
+	free(chunk);
+	out_treeLength = finalTreeLength;
+	return finalTree;
+}
+
+template <int TNoBlocks>
+int* fastqToGraphAndPrecleaned(std::fstream& fs, std::fstream& ts, int& out_treeLength)
 {
 	char* d_chunk;
 	int* d_tree;
@@ -187,6 +294,6 @@ int* precleanedJumpGPU(std::fstream& fs, std::fstream& ts, int& size)
 	gpuErrchk(cudaFree(d_treeLength));
 	free(chunk);
 	free(clearedChunk);
-	size = finalTreeLength;
+	out_treeLength = finalTreeLength;
 	return finalTree;
 }
